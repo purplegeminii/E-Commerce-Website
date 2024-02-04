3a4cb4704bc25ff800bbc6752d65caded87b709c
Delali Nsiah-Asare <delalinsiah.asare@gmail.com>
2024-02-04 19:00:56 +0000

    Update login.php

--- login/login.php
+++ login/login.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Log in</title>
+    <title>LOGIN</title>
 </head>
 <body>
-    <h1>Login Page</h1>
+    <h1>LOGIN</h1>
 </body>
 </html>
