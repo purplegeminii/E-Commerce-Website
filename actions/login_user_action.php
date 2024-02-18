e65a16fae694cba48f9883d0ee476cd27d7dea95
Delali Nsiah-Asare <delalinsiah.asare@gmail.com>
2024-02-18 22:06:27 +0000

    login/register

--- actions/login_user_action.php
+++ actions/login_user_action.php
@@ -2,42 +2,45 @@
 
 session_start();
 include "../settings/connection.php";
+global $conn;
 
-// if (isset($_POST['login-button'])) {}
-if ($_SERVER['HTTP_REFERER'] == "http://localhost/e-commerce-website/login/login.php") {
+// if (isset($_POST['login-button'])) {
+//if ($_SERVER['HTTP_REFERER'] == "../login/login_view.php") {
+if (isset($_POST['login-button'])) {
     $email = mysqli_real_escape_string($conn, $_POST['email-input']);
     $password = mysqli_real_escape_string($conn, $_POST['password-input']);
 
-    $query = "SELECT COUNT(*) AS email_count, passwd FROM people WHERE email = ?";
+    $query = "SELECT COUNT(*) AS email_count, pid, rid, passwd FROM People WHERE email = ?";
     $stmt = $conn->prepare($query);
     $stmt->bind_param("s", $email);
     $stmt->execute();
     $result = $stmt->get_result();
     $row = $result->fetch_assoc();
-    
+
     $emailCount = $row['email_count'];
     $hashedPasswordFromDatabase = $row['passwd'];
 
     if ($emailCount > 0) {
         // Verify the entered password against the stored hash
         if (password_verify($password, $hashedPasswordFromDatabase)) {
-            // Passwords match, login successful
-            header("location: ../view/home.php");
-            exit();
+            // Passwords match, login successful, set user_id & role_id
+            $_SESSION['user_id'] = $row['pid'];
+            $_SESSION['role_id'] = $row['rid'];
+            echo '<script>alert("login successful");</script>';
+            echo '<script>window.location.href="../view/dashboard.php";</script>';
         } else {
             // Passwords do not match, login failed
-            header("location: ../login/login.php");
-            exit();
+            echo '<script>alert("login failed");</script>';
+            echo '<script>window.location.href="../login/login_view.php";</script>';
         }
+        exit();
     } else {
         echo "Email does not exist in the database.";
-        header("location: ../login/login.php");
-        exit();
+        header("location: ../login/login_view.php");
     }
 
-    
 
-    $stmt->close();
+    mysqli_stmt_free_result($stmt);
     $conn->close();
 }
 
