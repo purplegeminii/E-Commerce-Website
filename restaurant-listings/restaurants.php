5ec98eb363cbb33464a5e917d1dec568374be6ae
Delali Nsiah-Asare <delalinsiah.asare@gmail.com>
2024-02-22 20:48:08 +0000

    php/restaurants

--- restaurant-listings/restaurants.php
+++ restaurant-listings/restaurants.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Link to the CSS file -->
-    <link href="../css/resturant-listings.css" rel="stylesheet" />
+    <link href="../css/restaurant-listings.css" rel="stylesheet" />
 
     <title>Restaurants</title>
 </head>
@@ -16,52 +16,52 @@
                 <span>Eats Elite</span>
                 <br />
             </h1>
-            <a href="../restaurantlistings/restaurants.php" class="header-restaurants">Restaurants</a>
+            <a href="../restaurant-listings/restaurants.php" class="header-restaurants">Restaurants</a>
             <a href="#" class="header-meals">Meals</a>
             <a href="../login/logout.php" class="header-logout"><span>Logout</span></a>
         </header>
-        <div class="resturant-listings-container">
-            <div class="resturant-listings-container01">
-                <div class="resturant-listings-container02">
-                    <h1 class="resturant-listings-text">Pick Your Take-Out Restaurant</h1>
-                    <div class="resturant-listings-container03">
-                        <div class="resturant-listings-container04">
-                            <button type="button" disabled="false" class="resturant-listings-button button">
+        <div class="restaurant-listings-container">
+            <div class="restaurant-listings-container01">
+                <div class="restaurant-listings-container02">
+                    <h1 class="restaurant-listings-text">Pick Your Take-Out Restaurant</h1>
+                    <div class="restaurant-listings-container03">
+                        <div class="restaurant-listings-container04">
+                            <button type="button" disabled="false" class="restaurant-listings-button button">
                                 Order Here
                             </button>
-                            <img src="../assets/external/image_2024-02-13_213104728.png" alt="image" class="resturant-listings-image" />
+                            <img src="../assets/external/image_2024-02-13_213104728.png" alt="image" class="restaurant-listings-image" />
                         </div>
-                        <div class="resturant-listings-container05">
-                            <button type="button" disabled="true" class="resturant-listings-button1 button">
+                        <div class="restaurant-listings-container05">
+                            <button type="button" disabled="true" class="restaurant-listings-button1 button">
                                 Order Here
                             </button>
-                            <img src="../assets/external/image_2024-02-13_213657362.png" alt="image" class="resturant-listings-image1" />
+                            <img src="../assets/external/image_2024-02-13_213657362.png" alt="image" class="restaurant-listings-image1" />
                         </div>
-                        <div class="resturant-listings-container06">
-                            <button type="button" disabled="true" class="resturant-listings-button2 button">
+                        <div class="restaurant-listings-container06">
+                            <button type="button" disabled="true" class="restaurant-listings-button2 button">
                                 Order Here
                             </button>
-                            <img src="../assets/external/image_2024-02-13_213615505.png" alt="image" class="resturant-listings-image2" />
+                            <img src="../assets/external/image_2024-02-13_213615505.png" alt="image" class="restaurant-listings-image2" />
                         </div>
                     </div>
-                    <div class="resturant-listings-container07">
-                        <div class="resturant-listings-container08">
-                            <button type="button" disabled="true" class="resturant-listings-button3 button">
+                    <div class="restaurant-listings-container07">
+                        <div class="restaurant-listings-container08">
+                            <button type="button" disabled="true" class="restaurant-listings-button3 button">
                                 Order Here
                             </button>
-                            <img src="../assets/external/image_2024-02-13_221749767.png" alt="image" class="resturant-listings-image3" />
+                            <img src="../assets/external/image_2024-02-13_221749767.png" alt="image" class="restaurant-listings-image3" />
                         </div>
-                        <div class="resturant-listings-container09">
-                            <button type="button" disabled="true" class="resturant-listings-button4 button">
+                        <div class="restaurant-listings-container09">
+                            <button type="button" disabled="true" class="restaurant-listings-button4 button">
                                 Order Here
                             </button>
-                            <img src="../assets/external/image_2024-02-13_230519548.png" alt="image" class="resturant-listings-image4" />
+                            <img src="../assets/external/image_2024-02-13_230519548.png" alt="image" class="restaurant-listings-image4" />
                         </div>
-                        <div class="resturant-listings-container10">
-                            <button type="button" disabled="true" class="resturant-listings-button5 button">
+                        <div class="restaurant-listings-container10">
+                            <button type="button" disabled="true" class="restaurant-listings-button5 button">
                                 Order Here
                             </button>
-                            <img src="../assets/external/image_2024-02-13_230432297.png" alt="image" class="resturant-listings-image5" />
+                            <img src="../assets/external/image_2024-02-13_230432297.png" alt="image" class="restaurant-listings-image5" />
                         </div>
                     </div>
                 </div>
