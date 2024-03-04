98d5ed8c4b602599978a0f0012e20cbe780bfe84
Delali Nsiah-Asare <delalinsiah.asare@gmail.com>
2024-03-04 13:56:41 +0000

    Update connection.php

--- settings/connection.php
+++ settings/connection.php
@@ -3,7 +3,7 @@
 $SERVER = "localhost";
 $USERNAME = "root";
 $PASSWD = "";
-$DATABASE = "food_db";
+$DATABASE = "EE2025";
 
 $conn = new mysqli($SERVER, $USERNAME, $PASSWD, $DATABASE) or die ("could not connect database");
 
