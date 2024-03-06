5b58e9c5d8e4243be510da20fb1004333cef0b2b
Delali Nsiah-Asare <delalinsiah.asare@gmail.com>
2024-03-06 16:47:27 +0000

    menu stuff

--- js/restaurant-listings.js
+++ js/restaurant-listings.js
@@ -1,22 +1,5 @@
 function goToMenu(id) {
-    alert("goto menu function");
-
-    // Create a FormData object to send data in the request body
-    const formData = new FormData();
-    formData.append('id', id);
-
-    // fetch api with POST method
-    fetch('../actions/goto_menu_action.php', {
-        method: 'POST',
-        body: formData
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Success:', data);
-        // Handle the response data as needed
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        // Handle errors
-    });
+    if (id === 1) {
+        window.location.href = '../menu/kfcMenu.php';
+    }
 }
