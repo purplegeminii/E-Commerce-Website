8fe02a5d62b687b17a7888be5168c71cc5ca88fc
Delali Nsiah-Asare <delalinsiah.asare@gmail.com>
2024-03-06 21:11:16 +0000

    Update index.php
    redirection for login page php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
                     <li><a href="#about-us">ABOUT US</a></li>
                     <li><a href="#reviews">OUR CUSTOMERS</a></li>
                     <li><a href="#contact-us">CONTACT US</a></li>
-                    <li><a class="signup" href="../login/register.php">SIGN UP</a></li>
+                    <li><a class="signup" href="login/register.php">SIGN UP</a></li>
                 </ul>      
         </div>
     </nav>
