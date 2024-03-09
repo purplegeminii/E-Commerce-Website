d5caa787416abc0c030e8f47262a82c18e00ff6c
Delali Nsiah-Asare <delalinsiah.asare@gmail.com>
2024-03-09 15:24:09 +0000

    cart logic php/db

--- actions/add_to_cart_action.php
+++ actions/add_to_cart_action.php
@@ -1,12 +1,23 @@
 <?php
 session_start();
 include "../settings/connection.php";
+global $conn;
 
 $item_id = $_GET['item_id'];
 
 if (!isset($_SESSION['isNewOrder'])) {
     $cust_id = $_SESSION['user_id'];
     $rest_id = $_SESSION['rest_id'];
+
+    $sql = "INSERT INTO Orders (Customer_ID, Restaurant_ID) VALUES (?,?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ii", $cust_id, $rest_id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result) {
+        $query = "INSERT INTO Order_Items () VALUES ()";
+    }
 
     $_SESSION['isNewOrder'] = false;
 } else {
