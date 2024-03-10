ef9d6709f72ba314c2a0b400d555a9f50b970144
1gitboss <98659804+1gitboss@users.noreply.github.com>
2024-03-10 14:08:13 +0000

    Modified login page design

--- login/login.php
+++ login/login.php
@@ -4,30 +4,47 @@
     <meta charset='UTF-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <title>Login page</title>
-    <link rel='stylesheet' href='../css/login.css'>
+    <link rel='stylesheet' href='login.css'>
 </head>
 <body>
-    <div class="top-banner">
-        <p>Eats Elite</p>
-    </div>
     <div class="login-container">
-        <h2>Login</h2>
-        <hr>
-        <form id="loginForm" name="loginForm" action="../actions/login_user_action.php" method="POST">
-            <label for="username">Username:</label>
-            <input type="text" id="username" name="username" required>
 
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password" required>
+        <div class="login-picture"></div>
 
-<!--            <label for="remember">Remember Password:</label>-->
-<!--            <input type="checkbox" id="remember" name="remember">-->
+        <div class="login-form">
+           
+            <div id="company-logo">
+                <img src="logo-transformed.png" id="logo" alt="company-logo">
+            </div>
 
-            <button type="submit" name="login-button" id="login-btn">Login</button>
-        </form>
-        <p id="error-message"></p>
-<!--        <p class="forgot-password"><a href="#" onclick="forgotPassword()">Forgot Password?</a></p>-->
-        <p class="sign-up">Don't have an account? <a href="../login/register.php">Sign up</a></p>
+            <div>
+                <h2>
+                    Welcome to <span>Eats Elite</span>
+               </h2> <br>
+
+               <h2>
+                    Login <span>Here</span>
+                </h2>
+
+            </div>
+            <br><br>
+            
+            <form id="loginForm" name="loginForm" action="../actions/login_user_action.php" method="POST">
+                 <label for="username">Email or Username:</label><br>
+                <input type="text" class="text-input" id="username" name="username" placeholder="john@example.com" required>
+                <br><br>
+    
+                <label for="password">Password:</label><br>
+                <input type="password" class="text-input" id="password" name="password" placeholder="********" required>
+    
+                <br> <br>
+    
+                <button type="submit" name="login-button" id="login-btn">LOGIN</button>
+            </form>
+            <br>
+            <p class="sign-up">Don't have an account? <a href="../login/register.php">Sign up</a></p>
+        </div>
+    
     </div>
     <script src="../js/login.js"></script>
 </body>
