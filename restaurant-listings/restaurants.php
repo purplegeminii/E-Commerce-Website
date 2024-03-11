f80ebdca1967ed089f63312c41d906a0f8671b3a
1gitboss <98659804+1gitboss@users.noreply.github.com>
2024-03-11 04:17:10 +0000

    modified restaurant listings

--- restaurant-listings/restaurants.php
+++ restaurant-listings/restaurants.php
@@ -1,79 +1,86 @@
-<?php
-include "../settings/core.php";
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <!-- Link to the CSS file -->
-    <link href="../css/restaurant-listings.css" rel="stylesheet" />
+    <link rel="stylesheet" href="restaurant-listings.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
     <title>Restaurants</title>
+
 </head>
 <body>
-    <div class="container">
-        <header class="header-header">
-            <h1 class="header-text">
-                <span>Eats Elite</span>
-                <br />
-            </h1>
-            <a href="../restaurant-listings/restaurants.php" class="header-restaurants">Restaurants</a>
-            <a href="#" class="header-meals">Meals</a>
-            <a href="../login/logout.php" class="header-logout"><span>Logout</span></a>
-        </header>
-        <div class="restaurant-listings-container">
-            <div class="restaurant-listings-container01">
-                <div class="restaurant-listings-container02">
-                    <h1 class="restaurant-listings-text">Pick Your Take-Out Restaurant</h1>
-                    <div class="restaurant-listings-container03">
-                        <div class="restaurant-listings-container04">
-                            <button type="button" class="restaurant-listings-button button" onclick="goToMenu(1)">
-                                Order Here
-                            </button>
-                            <img src="../assets/external/image_2024-02-13_213104728.png" alt="image" class="restaurant-listings-image" />
+ <nav>
+    <div class="header-container">
+        <div class="logo">
+             <!-- Logo goes here -->
+             <a href="index.php"><img height="auto" width="60px" src="assets/images/logo-transformed.png" alt="logo"></a>
+        </div> 
+         
+             <ul id="menu">
+                 <li><a href="#home">HOME</a></li>
+                 <div class="dropdown">
+                    <li><a href="#"><i class='fas'>&#xf406;</i> USER</a></li>
+                        <div class="dropdown-content">
+                            <a href="#">Item History</a>
+                            <a href="#">User Info</a>
+                            <a href="#">Logout</a>
                         </div>
+                 </div>
+                  <li><a href="#"><i class="fa fa-shopping-cart"></i> Cart</a> </li>   
+             </ul>      
+     </div>
+ </nav>
 
-                        <div class="restaurant-listings-container05">
-                            <button type="button" disabled="true" class="restaurant-listings-button1 button">
-                                Order Here
-                            </button>
-                            <img src="../assets/external/image_2024-02-13_213657362.png" alt="image" class="restaurant-listings-image1" />
-                        </div>
-                        <div class="restaurant-listings-container06">
-                            <button type="button" disabled="true" class="restaurant-listings-button2 button">
-                                Order Here
-                            </button>
-                            <img src="../assets/external/image_2024-02-13_213615505.png" alt="image" class="restaurant-listings-image2" />
-                        </div>
-                    </div>
-                    <div class="restaurant-listings-container07">
-                        <div class="restaurant-listings-container08">
-                            <button type="button" disabled="true" class="restaurant-listings-button3 button">
-                                Order Here
-                            </button>
-                            <img src="../assets/external/image_2024-02-13_221749767.png" alt="image" class="restaurant-listings-image3" />
-                        </div>
-                        <div class="restaurant-listings-container09">
-                            <button type="button" disabled="true" class="restaurant-listings-button4 button">
-                                Order Here
-                            </button>
-                            <img src="../assets/external/image_2024-02-13_230519548.png" alt="image" class="restaurant-listings-image4" />
-                        </div>
-                        <div class="restaurant-listings-container10">
-                            <button type="button" disabled="true" class="restaurant-listings-button5 button">
-                                Order Here
-                            </button>
-                            <img src="../assets/external/image_2024-02-13_230432297.png" alt="image" class="restaurant-listings-image5" />
-                        </div>
-                    </div>
-                </div>
+
+    <section class="listing">
+        <div>
+            <div class="stat-box Kfc">
+                <a href="../menu/kfcMenu.php">
+                    <img src="../assets/images/KFC-img.png"  alt="logo">
+                </a>
+            </div>
+            
+            
+
+        </div>
+
+        <div>
+
+            <div class="stat-box munchies">
+                <a href="#">
+                    <img src="../assests/images/munchies-img.png"  alt="logo">
+                </a>
+            </div>
+
+
+        </div>
+
+        <div>
+            <div class="stat-box sushi">
+                <a href="#">
+                    <img src="../assets/images/sushi-img.png"  alt="logo">
+                </a>       
             </div>
         </div>
-        
-    </div>
-    <script type="text/javascript" src="../js/restaurant-listings.js"></script>
+
+       <div>
+        <h3>KFC</h3>
+       </div>
+
+       <div>
+        <h3>Munchies</h3>
+       </div>
+
+       <div>
+        <h3>Sushi Today</h3>
+       </div>
+
+    </section>
+
+    <footer>
+        <p>&copy; 2024 Eats Elite. All rights reserved.</p>
+    </footer>
 </body>
 </html>
