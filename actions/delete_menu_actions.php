61e18651102946faca1f62b5ab6d61a60cd7a848
Janet-elorm <103153739+Janet-elorm@users.noreply.github.com>
2024-03-17 22:18:44 +0000

    Add files via upload

---
+++ actions/delete_menu_actions.php
@@ -0,0 +1,19 @@
+<?php
+include "../settings/connection.php";
+
+if (isset($_GET['id'])) {
+    $menuId = $_GET['id'];
+
+   
+
+    $query = "DELETE FROM menu_items WHERE Item_ID = $menuId";
+
+     if (mysqli_query($conn, $query)) {
+         header('Location: ../admin/management.php');
+         exit();
+     } else {
+         echo "Error: " . $query . "<br>" . mysqli_error($conn);
+     }
+} 
+
+?>
