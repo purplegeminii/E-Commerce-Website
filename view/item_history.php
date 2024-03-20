26e8c3defa7d548eba789c9bc9e7e124e14ac225
1gitboss <98659804+1gitboss@users.noreply.github.com>
2024-03-20 06:10:45 +0000

    updated front-end

--- view/item_history.php
+++ view/item_history.php
@@ -14,25 +14,28 @@ $items = get_item_history($_SESSION['user_id']);
 </head>
 <body>
     <div id="content">
-        <h1>Item History</h1>
-        <?php
-        if ($items) {
-            echo '<table>';
-            echo '<tr><th>Date</th><th>Item Name</th><th>Price</th><th>Quantity</th><th>Subtotal</th></tr>';
-            foreach ($items as $item) {
-                echo '<tr>';
-                echo '<td>' . $item['Order_Date_Time'] . '</td>';
-                echo '<td>' . $item['Item_Name'] . '</td>';
-                echo '<td>' . $item['Item_Price'] . '</td>';
-                echo '<td>' . $item['Quantity'] . '</td>';
-                echo '<td>' . $item['Subtotal'] . '</td>';
-                echo '</tr>';
+        <div class="container">
+            <h1>Item History</h1>
+            <?php
+            if ($items) {
+                echo '<table>';
+                echo '<tr><th>Date</th><th>Item Name</th><th>Price</th><th>Quantity</th><th>Subtotal</th></tr>';
+                foreach ($items as $item) {
+                    echo '<tr>';
+                    echo '<td>' . $item['Order_Date_Time'] . '</td>';
+                    echo '<td>' . $item['Item_Name'] . '</td>';
+                    echo '<td>' . $item['Item_Price'] . '</td>';
+                    echo '<td>' . $item['Quantity'] . '</td>';
+                    echo '<td>' . $item['Subtotal'] . '</td>';
+                    echo '</tr>';
+                }
+                echo '</table>';
+            } else {
+                echo "<p>You have no items in your history.</p>";
             }
-            echo '</table>';
-        } else {
-            echo "<p>You have no items in your history.</p>";
-        }
-        ?>
+            ?>
+
+        </div>
     </div>
 </body>
 </html>
--- view/user_profile.php
+++ view/user_profile.php
@@ -17,36 +17,39 @@ $user_info = get_user_info();
 </head>
 <body>
 <div id="content">
-    <h1>User Profile</h1>
-    <form id="profileForm">
-        <h2>Role: <?= $user_info['role'] ?></h2>
-        <section class="personal-info">
-            <h2>Personal Information</h2>
-            <input type="text" id="fname" name="fname" placeholder="First Name" value="<?= $user_info['fname'] ?>">
-            <input type="text" id="lname" name="lname" placeholder="Last Name" value="<?= $user_info['lname'] ?>">
-            <select id="gender" name="gender">
-                <option value="<?= $user_info['gender'] ?>">Male</option>
-                <?php
-                if ($user_info['gender'] == "Female") {
-                    echo "<option value='Male'>Male</option>";
-                } else {
-                    echo "<option value='Female'>Female</option>";
-                }
-                ?>
-            </select>
-            <input type="date" id="dob" name="dob" value="<?= $user_info['dob'] ?>">
-        </section>
-        <section class="contact-info">
-            <h2>Contact Information</h2>
-            <input type="email" id="email" name="email" placeholder="Email" value="<?= $user_info['email'] ?>">
-            <input type="tel" id="tel" name="tel" placeholder="Phone" value="<?= $user_info['tel'] ?>">
-        </section>
-        <section class="address">
-            <h2>Address</h2>
-            <input type="text" id="address" name="address" placeholder="Address" value="<?= $user_info['address'] ?>">
-        </section>
-        <input type="submit" value="Save" id="saveBtn">
-    </form>
+    <div class="form-container">
+        <h1>User Profile</h1>
+        <form id="profileForm">
+            <!--        <h2>Role: --><?php //= $user_info['role'] ?><!--</h2>-->
+            <section class="personal-info">
+                <h2>Personal Information</h2>
+                <input type="text" id="fname" name="fname" placeholder="First Name" value="<?= $user_info['fname'] ?>">
+                <input type="text" id="lname" name="lname" placeholder="Last Name" value="<?= $user_info['lname'] ?>">
+                <select id="gender" name="gender">
+                    <option value="<?= $user_info['gender'] ?>">Male</option>
+                    <?php
+                    if ($user_info['gender'] == "Female") {
+                        echo "<option value='Male'>Male</option>";
+                    } else {
+                        echo "<option value='Female'>Female</option>";
+                    }
+                    ?>
+                </select>
+                <input type="date" id="dob" name="dob" value="<?= $user_info['dob'] ?>">
+            </section>
+            <section class="contact-info">
+                <h2>Contact Information</h2>
+                <input type="email" id="email" name="email" placeholder="Email" value="<?= $user_info['email'] ?>">
+                <input type="tel" id="tel" name="tel" placeholder="Phone" value="<?= $user_info['tel'] ?>">
+            </section>
+            <section class="address">
+                <h2>Address</h2>
+                <input type="text" id="address" name="address" placeholder="Address" value="<?= $user_info['address'] ?>">
+            </section>
+            <input type="submit" value="Save" id="saveBtn">
+        </form>
+
+    </div>
 </div>
 <script src="../js/user_profile.js"></script>
 </body>
